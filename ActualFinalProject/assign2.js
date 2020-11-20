90b1f0539c2c185938fb9f250ab7d3a1f3193e03
Grant Nagy <grantnagy11@gmail.com>
2020-11-20 09:34:08 -0700

    Fixed bubbles, new js and slightly new html/css

--- ActualFinalProject/assign2.js
+++ ActualFinalProject/assign2.js
@@ -1,19 +1,21 @@
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("Slides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
+  var id = ev.relatedTarget.id;
+  switch (id) {
+    case "1":
+      $(".Slides1").show();
+      $(".Slides2").hide();
+      $(".Slides3").hide();
+      break;
+    case "2":
+      $(".Slides2").show();
+      $(".Slides1").hide();
+      $(".Slides3").hide();
+      break;
+    case "3":
+      $(".Slides3").show();
+      $(".Slides2").hide();
+      $(".Slides1").hide();
+      break;
   }
+})
