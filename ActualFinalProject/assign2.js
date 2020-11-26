89a29d4d2e54342cd796bf34986526b2fd6d25c0
Addie Meders <ameders123456789@gmail.com>
2020-11-26 13:57:08 -0700

    Functioning search with alert

--- ActualFinalProject/assign2.js
+++ ActualFinalProject/assign2.js
@@ -19,3 +19,12 @@
       break;
   }
 })
+
+
+$(document).ready(function() {
+  $("#searchButton")[0].addEventListener("click", searchItem);
+function searchItem(){
+  var keepMe = document.getElementById("searchInfo").value
+  alert("Search for " + keepMe + " was successful!")
+}
+});
